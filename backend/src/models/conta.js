3877232cb84940d938624d33dcde20d87311fe25
Flavio Alecio <flaviotubaroni@gmail.com>
2024-12-29 15:13:46 -0300

    Conta Model - development started

---
+++ backend/src/models/conta.js
@@ -0,0 +1,28 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize(databaseConfig);
+
+const Conta = sequelize.define(
+    'Conta',
+    {
+        idConta: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true
+        },
+        nome: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        senha: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    },
+    {
+        
+    },
+);
--- backend/src/models/m.md
+++ backend/src/models/m.md
@@ -1,1 +1,18 @@
 models aqui
+
+## Script PostgreSQL
+
+CREATE TABLE Contas
+(
+	idConta INTEGER NOT NULL PRIMARY KEY,
+	nome VARCHAR(30) NOT NULL,
+	email VARCHAR(80) NOT NULL,
+	senha VARCHAR(30) NOT NULL
+)
+
+CREATE TABLE Usuarios
+(
+	cpf CHAR(11) NOT NULL PRIMARY KEY,
+	idConta INTEGER NOT NULL,
+	FOREIGN KEY (idConta) REFERENCES Contas(idConta)
+)
