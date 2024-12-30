b4349abca6d3704ad4262fcbac1b5329ab461b48
VnszinN <147839981+VnszinN@users.noreply.github.com>
2024-12-30 11:33:07 -0300

    feat: added Reserva, Bibliotecario

---
+++ backend/src/models/Bibliotecario.js
@@ -0,0 +1,30 @@
+const { Sequelize, DataTypes, Model} = require('sequelize');
+const sequelize = new Sequelize(databaseConfig);
+
+const{Conta} = require("./conta");
+
+class Bibliotecario extends Model{
+    static init(sequelize){
+        super.init(
+            {
+                idConta:{
+                    type:DataTypes.INTEGER,
+                    references:{
+                        model: Conta,
+                        key: 'id',
+                    },
+                    primaryKey:true,
+                    allowNull:false,
+                    field: 'contaId'
+                }
+            },
+            {
+                sequelize,
+                tableName: 'Bibliotecario'
+            }
+        )
+    }
+}
+
+var Bibliotecario = sequelize.model("Binliotecario", Bibliotecario);
+module.exports = Bibliotecario;
