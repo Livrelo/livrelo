13522f3426303f5bd5fdd5e2b67e28f2078ddfd6
Ana Carolina Sá <anacarolina.sacoelho@gmail.com>
2025-01-06 16:24:53 -0300

    feat: add emprestimoRouter

--- backend/src/routes/emprestimoRoutes.js
+++ backend/src/routes/emprestimoRoutes.js
@@ -1,8 +1,13 @@
-import express from "express";
+import e from "express";
+import emprestimoController from "../controllers/emprestimoController";
 
+const emprestimoRouter = e.Router();
 
-const routes = express.Router();
+emprestimoRouter.get("/emprestimos", emprestimoController.findAll);
+emprestimoRouter.get("/emprestimos/:cpf", emprestimoController.findByCPF);
+emprestimoRouter.get("/emprestimos/:id", emprestimoController.findByID);
+emprestimoRouter.get("/emprestimosAtrasados", emprestimoController.findEmprestimosEmAtraso);
+emprestimoRouter.post("/emprestimo", emprestimoController.create);
+emprestimoRouter.put("/emprestimo/:id", emprestimoController.update);
 
-
-
-export default routes;
+export default emprestimoRouter;
