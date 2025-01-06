d14364bf9c53f9c6133b371cb05ef20b0fbf23f1
Ana Carolina Sá <anacarolina.sacoelho@gmail.com>
2025-01-06 17:23:22 -0300

    fix: fix devolucao Router

--- backend/src/routes/devolucaoRoutes.js
+++ backend/src/routes/devolucaoRoutes.js
@@ -4,7 +4,7 @@ import devolucaoController from "../controllers/devolucaoController";
 const devolucaoRouter = e.Router();
 
 devolucaoRouter.get("/devolucao", devolucaoController.findAll);
-devolucaoRouter.get("/devolucao/:id", devolucaoController.findByID);
+devolucaoRouter.get("/devolucao/:idEmprestimo", devolucaoController.findByID);
 devolucaoRouter.post("/devolucao", devolucaoController.create);
 
 export default devolucaoRouter;
