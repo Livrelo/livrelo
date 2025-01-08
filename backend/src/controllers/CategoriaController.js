77d04a265e4df0c26f0f5c55bfdaf0685a986063
caiosantosdev <caiosantosdev@gmail.com>
2025-01-08 12:43:22 -0300

    feat: create, update e delete Categoria Controller e Service.

--- backend/src/controllers/CategoriaController.js
+++ backend/src/controllers/CategoriaController.js
@@ -29,23 +29,51 @@ class CategoriaController{
     }
     static async create(req, res){
         try{
+            const categoria = req.body;
+            const categoriaCriada = await CategoriaService.create(categoria);
+
+            return res.status(201).send({
+                message: 'Categoria criada com sucesso',
+                categoria: categoriaCriada
+            })
 
         }catch(error){
-            
+            return res.status(400).send({
+                message: 'Ocorreu um erro ao criar categoria.',
+                error: error.message
+            })
         }
     }
     static async update(req, res){
         try{
-
+            const id = req.params.id;
+            const categoria = req.body;
+            const categoriaAtualizada = await CategoriaService.update(id, categoria);
+            return res.status(204).send({
+                message: 'Categoria atualizada com sucesso',
+                categoria: categoriaAtualizada
+            })
         }catch(error){
-            
+            return res.status(400).send({
+                message: 'Ocorreu um erro ao atualizar a categoria.',
+                error: error.message
+            })
         }
     }
     static async delete(req, res){
         try{
+            const id = req.params.id;
+            const categoriaDeletada = await CategoriaService.delete(id);
 
+            return res.status(200).send({
+                message: 'Categoria deletada com sucesso',
+                categoria: categoriaDeletada
+            })
         }catch(error){
-            
+            return res.status(400).send({
+                message: 'Ocorreu um erro ao deletar categoria',
+                error: error.message
+            })
         }
     }
 }
