5da40ee077c20140b240068f0ec4f28d7be1769f
Ana Carolina Sá <anacarolina.sacoelho@gmail.com>
2025-01-12 19:43:14 -0300

    feat: add navbar

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/usuario/tela-home/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
-
---
+++ frontend/src/pages/usuario/tela-home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../../../components/navbar/Navbar';
+
+function Home() {
+  return (
+    <div>
+      <Navbar />
+      <div style={{ padding: '50px' }}>
+        <h1>Teste home</h1>
+        <p>Teste teste</p>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
