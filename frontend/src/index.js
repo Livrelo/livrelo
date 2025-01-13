793080061dddc18849483147fd68203655d22101
VnszinN <147839981+VnszinN@users.noreply.github.com>
2025-01-13 14:25:59 -0300

    feat: create SignIn and SignUp pages

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/usuario/tela-home/Home';
 import Reservas from './pages/usuario/tela-minhas-reservas/Reservas';
+import SignIn from './pages/usuario/tela-signin/SignIn';
+import Signup from './pages/tela-signup/Signup';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -10,6 +12,8 @@ root.render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/reservas" element={<Reservas />} />
+        <Route path='/signin' element={<SignIn/>}/>
+        <Route path='/signup' element={<Signup/>}/>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
