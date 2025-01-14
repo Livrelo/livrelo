07f0fd236ad717a1f9ffef62fda1e4b45cb90f12
Ana Carolina Sá <anacarolina.sacoelho@gmail.com>
2025-01-13 22:28:33 -0300

    feat: add home do bibliotecario e dashboard

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import Home from './pages/usuario/tela-home/Home';
 import Reservas from './pages/usuario/tela-minhas-reservas/Reservas';
 import ReservarLivro from './pages/usuario/tela-reservar/ReservarLivro';
 import Emprestimos from './pages/usuario/tela-meus-emprestimos/Emprestimos';
+import HomeB from './pages/bibliotecario/tela-home-b/HomeB';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,6 +16,7 @@ root.render(
         <Route path="/reservas" element={<Reservas />} />
         <Route path="/reservar" element={<ReservarLivro />} />
         <Route path="/emprestimos" element={<Emprestimos />} />
+        <Route path="/home-b" element={<HomeB />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
