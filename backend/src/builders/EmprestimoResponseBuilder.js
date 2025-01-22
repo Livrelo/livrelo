42c83bee989da795402092dd4d69d83359f93c88
caiosantosdev <caiosantosdev@gmail.com>
2025-01-22 16:55:02 -0300

    added response builder to Emprestimo

--- backend/src/builders/EmprestimoResponseBuilder.js
+++ backend/src/builders/EmprestimoResponseBuilder.js
@@ -5,6 +5,10 @@ class EmprestimoResponseBuilder extends BaseBuilder {
     this.addData(emprestimos);
     return this;
   }
+  dataValues(){
+    this.response = this.response.map((item) => item.dataValues);
+    return this;
+  }
 }
 
 export default EmprestimoResponseBuilder;
