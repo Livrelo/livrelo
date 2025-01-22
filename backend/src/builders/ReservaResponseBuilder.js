e89f3188fc2b44638feb026179299dcbdf7f19d3
caiosantosdev <caiosantosdev@gmail.com>
2025-01-22 16:45:03 -0300

    added response builder to the Reserva

--- backend/src/builders/ReservaResponseBuilder.js
+++ backend/src/builders/ReservaResponseBuilder.js
@@ -5,6 +5,10 @@ class ReservaResponseBuilder extends BaseBuilder {
     this.addData(reservas);
     return this;
   }
+  dataValues(){
+    this.response = this.response.map((item) => item.dataValues);
+    return this;
+  }
 }
 
 export default ReservaResponseBuilder;
