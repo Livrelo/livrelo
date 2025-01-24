21b34c607d0c82588957d9c305a465544e753330
VnszinN <147839981+VnszinN@users.noreply.github.com>
2025-01-24 12:06:34 -0300

    fix: signIn and signUp css fixed

--- frontend/src/pages/usuario/tela-signup/Signup.jsx
+++ frontend/src/pages/usuario/tela-signup/Signup.jsx
@@ -1,97 +1,140 @@
-import * as React from 'react';
-import { useNavigate } from 'react-router-dom'; 
-import Header from "../../../components/header-signup/Header.jsx"
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import { createTheme, ThemeProvider } from '@mui/material/styles';  
-import img from './bg.jpg';
-import "./styleSignup.css"
+import * as React from "react";
+import { useNavigate } from "react-router-dom";
+import Header from "../../../components/header-signup/Header.jsx";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+import Input from "../../../components/Input/Input";
+import { Button, Typography, Box } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import TextMaskCustom from "../../../components/Input/maskinput";
+import img from "./bg.jpg";
+import "./styleSignup.css";
 
-function Signup(){
-    const theme = createTheme({
-        palette: {
-          blues: {
-            main: '#2a4fa0',
-            light: '#162E62',
-            dark: '#112757',
-            contrastText: '#ffffff',
-          },
-          whites: {
-            main: '#162E62',
-            dark: '#fffffff',
-            light: '#ffffff',
-            contrastText: '#162E62',
-          },
-        },
-      });
-      const navigate = useNavigate(); 
- return(  
-    <div className='Signup'>
-        <Header/>
-            <div className='Signup-Container'>
-            <img src={img} alt='SignImg'className='SignImg'></img>
-                <div className="wrap-form-SignUp">
-                    <form className="Signup-form">
-                        <span className="tittle-SignUp">Cadastro</span>
-                        <Box component="div" sx={{ '& > :not(style)': { m: 1, width: '75ch' } }} noValidate autoComplete="off"
-                        marginX={8}
-                        marginY={1}
-                        >
-                            <ThemeProvider theme={theme}>
-                                <TextField id="outlined-basic" label="CPF" color='blues'  variant="outlined" margin="normal" />
-                            </ThemeProvider>
-                            
-                        </Box>
-                        <Box component="div" sx={{ '& > :not(style)': { m: 1, width: '75ch' } }} noValidate autoComplete="off"
-                        marginX={8}
-                        marginY={1}
-                        >   
-                            <ThemeProvider theme={theme}>
-                                <TextField id="outlined-basic" label="Nome" color='blues' variant="outlined" margin="normal"/>
-                            </ThemeProvider>
-                        </Box>
-                        <Box component="div" sx={{ '& > :not(style)': { m: 1, width: '75ch' } }} noValidate autoComplete="off"
-                        marginX={8}
-                        marginY={1}
-                        >
-                            <ThemeProvider theme={theme}>
-                                <TextField id="outlined-basic" label="E-mail" color='blues'  variant="outlined" margin="normal" />
-                            </ThemeProvider>
-                            
-                        </Box>
-                        <Box component="div" sx={{ '& > :not(style)': { m: 1, width: '75ch' } }} noValidate autoComplete="off"
-                        marginX={8}
-                        marginY={1}
-                        >   
-                            <ThemeProvider theme={theme}>
-                                <TextField id="outlined-basic" label="Senha" color='blues' variant="outlined" margin="normal"/>
-                            </ThemeProvider>
-                        </Box>
-                        <Box component="div" sx={{ '& > :not(style)': { m: 1, width: '30ch' } }} noValidate autoComplete="off"
-                        marginX={30}
-                        marginY={5}
-                        >   
-                            <ThemeProvider theme={theme}>
-                                <Button variant="contained" color="blues" size='large'>Cadastrar</Button>
-                            </ThemeProvider>
-                        </Box>
+function Signup() {
+  const theme = createTheme({
+    palette: {
+      blues: {
+        main: "#2a4fa0",
+        light: "#162E62",
+        dark: "#112757",
+        contrastText: "#ffffff",
+      },
+      whites: {
+        main: "#162E62",
+        dark: "#fffffff",
+        light: "#ffffff",
+        contrastText: "#162E62",
+      },
+    },
+  });
+  const initialValues = {
+    CPF: "",
+    nome: "",
+    email: "",
+    senha: "",
+  };
 
-                        <span className="label-SignIn">Já possui conta?</span>  
+  const validationSchema = Yup.object({
+    CPF: Yup.string()
+      .required("O CPF é obrigatório.")
+      .matches(/^.{14}$/, "O CPF deve ter 11 dígitos."),
+    nome: Yup.string().required("O nome é obrigatório"),
+    email: Yup.string()
+      .required("O email é obrigatório.")
+      .email("Coloque um email válido"),
+    senha: Yup.string().required("A senha é obrigatória."),
+  });
+  const handleSubmit = (values) => {
+    console.log(values);
+  };
+  const navigate = useNavigate();
+  return (
+    <div className="Signup">
+      <Header />
+      <div className="Signup-Container">
+        <img src={img} alt="SignImg" className="SignImg"></img>
+        <div className="wrap-form-SignUp">
+          <Box className="Signup-form">
+            <Typography variant="h6" className="tittle-SignUp">
+              Cadastro
+            </Typography>
+            <Formik
+              initialValues={{ ...initialValues }}
+              validationSchema={validationSchema}
+              onSubmit={(values) => {
+                handleSubmit(values);
+              }}
+            >
+              {({ isValid, dirty }) => (
+                <Form>
+                  <Input
+                    name="CPF"
+                    label="CPF"
+                    placeholder="Digite seu CPF"
+                    type="text"
+                    slotProps={{
+                      input: {
+                        inputComponent: TextMaskCustom,
+                      },
+                    }}
+                    size="small"
+                  />
+                  <Input
+                    name="nome"
+                    label="nome"
+                    placeholder="Digite seu nome"
+                    type="text"
+                    size="small"
+                  />
+                  <Input
+                    name="email"
+                    label="e-mail"
+                    placeholder="Digite seu e-mail"
+                    type="text"
+                    size="small"
+                  />
+                  <Input
+                    name="senha"
+                    label="senha"
+                    placeholder="Digite sua senha"
+                    type="text"
+                    size="small"
+                  />
+                  <Box
+                    component="div"
+                    className="btn-signUp-div"
+                  >
+                    <ThemeProvider theme={theme}>
+                      <Button className="signUp-botao" variant="contained" color="blues" size="large">
+                        Cadastrar
+                      </Button>
+                    </ThemeProvider>
+                  </Box>
+                </Form>
+              )}
+            </Formik>
+          </Box>
+          <span className="label-ToSignIn">Já possui conta?</span>
 
-                        <Box component="div" sx={{ '& > :not(style)': { m: 1, width: '25ch' } }} noValidate autoComplete="off"
-                        marginX={32.5}
-                        marginY={0}
-                        >   
-                            <ThemeProvider theme={theme}>
-                                <Button variant="outlined" color="whites" size='large'onClick={()=>navigate("/signin")}>Entrar</Button>
-                            </ThemeProvider>
-                        </Box> 
-                    </form>
-                </div>
-
-            </div>
+          <Box
+            component="div"
+           className="btn-signUp-div"
+          >
+            <ThemeProvider theme={theme}>
+              <Button
+                className="toSignIn-botao"
+                variant="outlined"
+                color="whites"
+                size="large"
+                onClick={() => navigate("/signin")}
+              >
+                Entrar
+              </Button>
+            </ThemeProvider>
+          </Box>
+        </div>
+      </div>
     </div>
- );
+  );
 }
 export default Signup;
