cf3f4857d5b0b13d25608702ce2eeea60e367bcd
Ana Carolina Sá <anacarolina.sacoelho@gmail.com>
2025-01-25 22:34:49 -0300

    feat: add modal para registrar devolucao de emprestimo

--- frontend/src/pages/bibliotecario/tela-emprestimos/EmprestimosB.jsx
+++ frontend/src/pages/bibliotecario/tela-emprestimos/EmprestimosB.jsx
@@ -10,12 +10,12 @@ import "./styles.css";
 
 export default function EmprestimosB() {
     const emprestimos = [
-        { id: 1, usuario: "fulano da silva", tituloLivro: "Dom Casmurro", dataEmprestimo: "10/01/2025", dataFim: "20/01/2025", status: "Ativo", dataDevolucao: "" },
-        { id: 2, usuario: "harry potter da silva", tituloLivro: "1984", dataEmprestimo: "05/01/2025", dataFim: "15/01/2025", status: "Concluído", dataDevolucao: "14/01/2025" },
-        { id: 3, usuario: "caio santos dev", tituloLivro: "O Hobbit", dataEmprestimo: "12/01/2025", dataFim: "18/01/2025", status: "Atrasado", dataDevolucao: "" },
-        { id: 4, usuario: "carolzinha", tituloLivro: "Orgulho e Preconceito", dataEmprestimo: "18/01/2025", dataFim: "30/01/2025", status: "Ativo", dataDevolucao: "" },
-        { id: 5, usuario: "banguela gatao", tituloLivro: "A Revolução dos Bichos", dataEmprestimo: "02/01/2025", dataFim: "18/01/2025", status: "Atrasado", dataDevolucao: "" },
-        { id: 6, usuario: "marcelo areas", tituloLivro: "O Senhor dos Anéis", dataEmprestimo: "02/01/2025", dataFim: "18/01/2025", status: "Atrasado", dataDevolucao: "" },
+        { id: 1, usuario: "fulano da silva", idLivro: 1, tituloLivro: "Dom Casmurro", dataEmprestimo: "10/01/2025", dataFim: "20/01/2025", status: "Ativo", dataDevolucao: "" },
+        { id: 2, usuario: "harry potter da silva", idLivro: 2, tituloLivro: "1984", dataEmprestimo: "05/01/2025", dataFim: "15/01/2025", status: "Concluído", dataDevolucao: "14/01/2025" },
+        { id: 3, usuario: "caio santos dev", idLivro: 3, tituloLivro: "O Hobbit", dataEmprestimo: "12/01/2025", dataFim: "18/01/2025", status: "Atrasado", dataDevolucao: "" },
+        { id: 4, usuario: "carolzinha", idLivro: 4, tituloLivro: "Orgulho e Preconceito", dataEmprestimo: "18/01/2025", dataFim: "30/01/2025", status: "Ativo", dataDevolucao: "" },
+        { id: 5, usuario: "banguela gatao", idLivro: 5, tituloLivro: "A Revolução dos Bichos", dataEmprestimo: "02/01/2025", dataFim: "18/01/2025", status: "Atrasado", dataDevolucao: "" },
+        { id: 6, usuario: "marcelo areas", idLivro: 6, tituloLivro: "O Senhor dos Anéis", dataEmprestimo: "02/01/2025", dataFim: "18/01/2025", status: "Atrasado", dataDevolucao: "" },
     ];
 
     const [openModal, setOpenModal] = useState(false);
