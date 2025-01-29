09671a266ec02202501cd7185297b4c962117016
Ana Carolina Sá <anacarolina.sacoelho@gmail.com>
2025-01-28 23:51:34 -0300

    feat: add modal para confirmação de cancelamento de reserva

---
+++ frontend/src/components/modal/ModalCancelarReserva.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Dialog, DialogTitle, DialogContent, DialogActions, Button, Typography } from "@mui/material";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+
+import "./styles.css";
+
+
+
+export default function ModalCancelarReserva({ open, handleClose, idReserva, titulo }) {
+    const initialValues = {
+        titulo: titulo || "",
+        idReserva: idReserva || "",
+    };
+
+
+    const handleSubmit = (values, { resetForm }) => {
+        resetForm();
+        handleClose();
+        //cancela reserva utilizando o id da reserva passado pelo cardddd
+    };
+
+    return (
+        <Dialog open={open} onClose={handleClose} maxWidth="sm" fullWidth initialValues={initialValues}
+            onSubmit={handleSubmit}>
+            <DialogTitle className="titulo">Cancelar reserva</DialogTitle>
+            <DialogContent>
+                <Typography className="livro-reservado-titulo">
+                    Tem certeza que deseja cancelar o livro {titulo}?
+                </Typography>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleClose} className="btn-cancelar">
+                    Voltar
+                </Button>
+                <Button
+                    type="submit"
+                    className="btn-registrar"
+
+                >
+                    Confirmar
+                </Button>
+            </DialogActions>
+        </Dialog >
+    );
+}
