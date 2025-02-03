4ba346b0eb78671193b6ac20b1fcd7d840e43ac2
VnszinN <147839981+VnszinN@users.noreply.github.com>
2025-02-03 11:27:32 -0300

    fix: fetch emprestimos working at emprestimos pages

--- frontend/src/pages/bibliotecario/tela-reservas/ReservasB.jsx
+++ frontend/src/pages/bibliotecario/tela-reservas/ReservasB.jsx
@@ -32,54 +32,6 @@ export default function ReservasB() {
         }
         setReservaRows(reservasArray);
     }, [reservas])
-    // const reservas = [
-    //     {
-    //         id: 1,
-    //         usuario: "Fulano da Silva",
-    //         idLivro: 1,
-    //         tituloLivro: "Dom Casmurro",
-    //         dataRetirada: "2025-01-20",
-    //         prazo: "2025-01-22",
-    //         status: "Ativa",
-    //     },
-    //     {
-    //         id: 2,
-    //         usuario: "Harry Potter",
-    //         idLivro: 2,
-    //         tituloLivro: "O Senhor dos Anéis",
-    //         dataRetirada: "2025-01-18",
-    //         prazo: "2025-01-20",
-    //         status: "Concluída",
-    //     },
-    //     {
-    //         id: 3,
-    //         usuario: "Maria Joaquina",
-    //         idLivro: 3,
-    //         tituloLivro: "1984",
-    //         dataRetirada: "2025-01-15",
-    //         prazo: "2025-01-17",
-    //         status: "Encerrada por prazo",
-    //     },
-    //     {
-    //         id: 4,
-    //         usuario: "Cirilo",
-    //         idLivro: 4,
-    //         tituloLivro: "O Hobbit",
-    //         dataRetirada: "2025-01-22",
-    //         prazo: "2025-01-24",
-    //         status: "Ativa",
-    //     },
-    //     {
-    //         id: 5,
-    //         usuario: "Ana Carolina",
-    //         idLivro: 5,
-    //         tituloLivro: "A Revolução dos Bichos",
-    //         dataRetirada: "2025-01-19",
-    //         prazo: "2025-01-21",
-    //         status: "Ativa",
-    //     },
-    // ];
-
 
     const [openModal, setOpenModal] = useState(false);
 
